3a4327a5f6cabb2d31b809802371880a80945625
Shruthi <yadandlashruthi97@gmail.com>
2025-01-30 16:00:44 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css'
 import './Responsive.css'
 import { TodoList } from './components/TodoList'
-// import { Routes, BrowserRouter as Router, Route } from "react-router-dom"
 
 const App = () => {
 
   return (
     <>
-      {/* <Router>
-        <Routes>
-          <Route path="/" element={<TodoList />} ></Route>
-        </Routes>
-      </Router> */}
       <TodoList />
     </>
   )
